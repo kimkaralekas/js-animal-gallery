b65db8e3322e949552604a658e2276fb82e6c5b0
kim <kimkaralekas@gmail.com>
2014-07-13 08:44:23 -0500

    button attempt

--- bearded-octo-ninja-master/gallery/script.js
+++ bearded-octo-ninja-master/gallery/script.js
@@ -1,1 +1,17 @@
 // Add your code here
+$("#.filter-button").on("click", function(){
+	$(this).toggleClass("active");
+	$(".monkey").toggle();
+});
+
+$("#dog").on("click", function(){
+	$(this).toggleClass("active");
+	$(".dog").toggle();
+});
+
+$("#cat").on("click", function(){
+	$(this).toggleClass("active");
+	$(".cat").toggle();
+});
+
+ 
